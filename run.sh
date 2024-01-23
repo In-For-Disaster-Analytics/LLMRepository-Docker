39cd8c6efc86287d33175c4ca424a847a7da5a9d
Maximiliano Osorio <maxiosorio@gmail.com>
2024-01-23 19:48:22 -0300

    fix: improve script

--- run.sh
+++ run.sh
@@ -1,224 +1,245 @@
 #!/bin/bash
+
+function install_conda() {
+	## Install miniconda
+	echo "Checking if miniconda3 is installed..."
+	if [ ! -d "$WORK/miniconda3" ]; then
+		echo "Miniconda not found in $WORK..."
+		echo "Installing..."
+		mkdir -p "$WORK/miniconda3"
+		curl https://repo.anaconda.com/miniconda/Miniconda3-py311_23.10.0-1-Linux-x86_64.sh -o "$WORK/miniconda3/miniconda.sh"
+		bash "$WORK/miniconda3/miniconda.sh" -b -u -p "$WORK/miniconda3"
+		rm -rf "$WORK/miniconda3/miniconda.sh"
+		export PATH="$WORK/miniconda3/bin:$PATH"
+		echo "Ensuring conda base environment is OFF..."
+		conda config --set auto_activate_base false
+	else
+		export PATH="$WORK/miniconda3/bin:$PATH"
+	fi
+	conda init bash
+	echo "Sourcing .bashrc..."
+	source ~/.bashrc
+	unset PYTHONPATH
+}
+
+function load_cuda() {
+	echo "Loading CUDA..."
+	module load cuda/12.0
+}
+
+function export_repo_variables() {
+	export COOKBOOK_NAME="sites-and-stories-nlp"
+	export COOKBOOK_DIR=${WORK}/cookbooks
+	export GIT_REPO_URL="https://github.com/In-For-Disaster-Analytics/sites-and-stories-nlp.git"
+	export GIT_BRANCH="jupyterenv"
+	export COOKBOOK_WORKSPACE_DIR=${COOKBOOK_DIR}/workspace/${COOKBOOK_NAME}
+	export COOKBOOK_ARCHIVE_DIR=${COOKBOOK_DIR}/archive/${COOKBOOK_NAME}
+	export COOKBOOK_ARCHIVE_PARENT_DIR=${COOKBOOK_DIR}/archive
+	export COOKBOOKS_WORKSPACE_PARENT_DIR=${COOKBOOK_DIR}/workspace
+	export UPDATE_AVAILABLE_FILE=${COOKBOOK_WORKSPACE_DIR}/UPDATE_AVAILABLE.txt
+    export NODE_HOSTNAME_PREFIX=$(hostname -s) # Short Host Name  -->  name of compute node: c###-###
+    export NODE_HOSTNAME_DOMAIN=$(hostname -d) # DNS Name  -->  stampede2.tacc.utexas.edu
+    export NODE_HOSTNAME_LONG=$(hostname -f)   # Fully Qualified Domain Name  -->  c###-###.stampede2.tacc.utexas.edu
+}
+
+function remove_update_available_file() {
+	if [ -f ${UPDATE_AVAILABLE_FILE} ]; then
+		rm ${UPDATE_AVAILABLE_FILE}
+	fi
+}
+
+function detect_update_available() {
+	git remote update
+	LAST_UPDATE=$(git show --no-notes --format=format:"%H" ${GIT_BRANCH} | head -n 1)
+	LAST_COMMIT=$(git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1)
+	if [ $LAST_COMMIT != $LAST_UPDATE ]; then
+		touch ${UPDATE_AVAILABLE_FILE}
+	fi
+}
+
+function clone_cookbook_on_workspace() {
+	if [ ! -d "$COOKBOOK_WORKSPACE_DIR" ]; then
+		git clone ${GIT_REPO_URL} --branch ${GIT_BRANCH} ${COOKBOOK_WORKSPACE_DIR}
+	else
+		detect_update_available
+	fi
+}
+
+function clone_cookbook_on_archive() {
+	if [ ! -d "${cookbook_archive_dir}" ]; then
+		mkdir -p ${cookbook_archive_dir}
+		git clone ${git_repo_url} --branch ${branch} ${cookbook_archive_dir}
+		chmod -R a-w ${cookbook_archive_dir}
+	else
+		chmod -R a+w ${cookbook_archive_dir}
+		git -C ${cookbook_archive_dir} pull origin ${branch}
+		chmod -R a-w ${cookbook_archive_dir}
+	fi
+}
+
+function init_directory() {
+	mkdir -p ${COOKBOOK_ARCHIVE_PARENT_DIR}
+	mkdir -p ${COOKBOOKS_WORKSPACE_PARENT_DIR}
+
+	remove_update_available_file
+	clone_cookbook_on_workspace
+	clone_cookbook_on_archive
+}
+
+function get_tap_certificate() {
+	mkdir -p ${HOME}/.tap # this should exist at this point, but just in case...
+	export TAP_CERTFILE=${HOME}/.tap/.${SLURM_JOB_ID}
+
+	# bail if we cannot create a secure session
+	if [ ! -f ${TAP_CERTFILE} ]; then
+		echo "TACC: ERROR - could not find TLS cert for secure session"
+		echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
+		exit 1
+	fi
+}
+
+function get_tap_token() {
+	# bail if we cannot create a token for the session
+	export TAP_TOKEN=$(tap_get_token)
+	if [ -z "${TAP_TOKEN}" ]; then
+		echo "TACC: ERROR - could not generate token for jupyter session"
+		echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
+		exit 1
+	fi
+	echo "TACC: using token ${TAP_TOKEN}"
+	export LOGIN_PORT=$(tap_get_port)
+}
+
+function load_tap_functions() {
+	TAP_FUNCTIONS="/share/doc/slurm/tap_functions"
+	if [ -f ${TAP_FUNCTIONS} ]; then
+		. ${TAP_FUNCTIONS}
+	else
+		echo "TACC:"
+		echo "TACC: ERROR - could not find TAP functions file: ${TAP_FUNCTIONS}"
+		echo "TACC: ERROR - Please submit a consulting ticket at the TACC user portal"
+		echo "TACC: ERROR - https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
+		echo "TACC:"
+		echo "TACC: job $SLURM_JOB_ID execution finished at: $(date)"
+		exit 1
+	fi
+}
+
+function create_jupyter_configuration {
+	TAP_JUPYTER_CONFIG="${HOME}/.tap/jupyter_config.py"
+	JUPYTER_SERVER_APP="ServerApp"
+	JUPYTER_BIN="jupyter-lab"
+	echo ${PWD}
+
+	cat <<-EOF >${TAP_JUPYTER_CONFIG}
+		# Configuration file for TAP jupyter session
+		import ssl
+		c = get_config()
+		c.IPKernelApp.pylab = "inline"  # if you want plotting support always
+		c.${JUPYTER_SERVER_APP}.ip = "0.0.0.0"
+		c.${JUPYTER_SERVER_APP}.port = $LOCAL_PORT
+		c.${JUPYTER_SERVER_APP}.open_browser = False
+		c.${JUPYTER_SERVER_APP}.allow_origin = u"*"
+		c.${JUPYTER_SERVER_APP}.ssl_options = {"ssl_version": ssl.PROTOCOL_TLSv1_2}
+		c.${JUPYTER_SERVER_APP}.root_dir = "${_tapisJobWorkingDir}"
+		c.${JUPYTER_SERVER_APP}.preferred_dir = "${_tapisJobWorkingDir}"
+		c.${JUPYTER_SERVER_APP}.notebook_dir = "${_tapisJobWorkingDir}/work"
+		c.FileContentsManager.delete_to_trash = False
+		c.IdentityProvider.token = "${TAP_TOKEN}"
+		c.MultiKernelManager.default_kernel_name = 'llm'
+	EOF
+
+}
+
+function run_jupyter() {
+	JUPYTER_ARGS="--certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG}"
+	nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &>${JUPYTER_LOGFILE} &
+	JUPYTER_PID=$!
+}
+
+function port_fowarding() {
+
+	# Port forwarding is set up for the four login nodes.
+	#
+	#   f: Requests ssh to go to background just before command execution.
+	#      Used if ssh asks for passwords but the user wants it in the background. Implies -n too.
+	#   g: Allows remote hosts to connect to local forwarded ports
+	#   N: Do not execute a remote command. Useful for just forwarding ports.
+	#   R: Connections to given TCP port/Unix socket on remote (server) host forwarded to local side.
+	#
+	# Create a reverse tunnel port from the compute node to the login nodes.
+	# Make one tunnel for each login so the user can just connect to stampede.tacc.utexas.edu.
+
+	# RUN JUPYTER SESSION IN BACKGROUND  -->  CAN STAY THE SAME
+	LOCAL_IPY_PORT=8888
+
+	# TAP Port
+	LOCAL_PORT=5902
+	# Disable exit on error so we can check the ssh tunnel status.
+	set +e
+	for i in $(seq 2); do
+		ssh -o StrictHostKeyChecking=no -q -f -g -N -R ${LOGIN_PORT}:${NODE_HOSTNAME_PREFIX}:${LOCAL_PORT} login${i}
+	done
+	if [ $(ps -fu ${USER} | grep ssh | grep login | grep -vc grep) != 2 ]; then
+		# jupyter will not be working today. sadness.
+		echo "TACC: ERROR - ssh tunnels failed to launch"
+		echo "TACC: ERROR - this is often due to an issue with your ssh keys"
+		echo "TACC: ERROR - undo any recent mods in ${HOME}/.ssh"
+		echo "TACC: ERROR - or submit a TACC consulting ticket with this error"
+		echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
+		exit 1
+	fi
+	# Re-enable exit on error.
+	set -e
+}
+
+function send_url_to_webhook() {
+	JUPYTER_URL="https://${NODE_HOSTNAME_DOMAIN}:${LOGIN_PORT}/?token=${TAP_TOKEN}"
+	INTERACTIVE_WEBHOOK_URL="${_webhook_base_url}"
+	# Wait a few seconds for jupyter to boot up and send webhook callback url for job ready notification.
+	# Notification is sent to _INTERACTIVE_WEBHOOK_URL, e.g. https://3dem.org/webhooks/interactive/
+	(
+		sleep 5 &&
+			curl -k --data "event_type=interactive_session_ready&address=${JUPYTER_URL}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" "${_INTERACTIVE_WEBHOOK_URL}" &
+	) &
+
+}
+
+function session_cleanup() {
+	# This file will be located in the directory mounted by the job.
+	SESSION_FILE=delete_me_to_end_session
+	touch $SESSION_FILE
+	echo $NODE_HOSTNAME_LONG $IPYTHON_PID >$SESSION_FILE
+	# While the session file remains undeleted, keep Jupyter session running.
+	while [ -f $SESSION_FILE ]; do
+		sleep 10
+	done
+}
+
+function install_dependencies() {
+	### Create env
+	if { conda env list | grep 'llm'; } >/dev/null 2>&1; then
+		conda activate llm
+	else
+		conda env create -n llm -f $cookbook_workspace_dir/.binder/environment.yml --force
+		conda activate llm
+		pip install --no-cache-dir -r $cookbook_workspace_dir/.binder/requirements.txt
+		python -m ipykernel install --user --name llm --display-name "Python (llm)"
+	fi
+}
+
+
 set -xe
-#Set Path to repo
-date_str=$(date +%Y-%m-%d-%H-%M-%S)
-cookbook_name="sites-and-stories-nlp"
-cookbooks_dir=${WORK}/cookbooks
-cookbook_archive_dir=${cookbooks_dir}/archive/${cookbook_name}/${date_str}
-export git_repo_working_dir=${cookbooks_dir}/${cookbook_name}
-
-echo "TACC: job ${SLURM_JOB_ID} execution at: $(date)"
-echo load cuda
-module load cuda/12.0
-
-# This file will be located in the directory mounted by the job.
-SESSION_FILE=delete_me_to_end_session
-touch $SESSION_FILE
-
-# RUN JUPYTER SESSION IN BACKGROUND  -->  CAN STAY THE SAME
-LOCAL_IPY_PORT=8888
-
-# TAP Port
-LOCAL_PORT=5902
-
-NODE_HOSTNAME_PREFIX=$(hostname -s)   # Short Host Name  -->  name of compute node: c###-###
-NODE_HOSTNAME_DOMAIN=$(hostname -d)   # DNS Name  -->  stampede2.tacc.utexas.edu
-NODE_HOSTNAME_LONG=$(hostname -f)     # Fully Qualified Domain Name  -->  c###-###.stampede2.tacc.utexas.edu
-
-echo "Checking if miniconda3 is installed..."
-if [ ! -d "$WORK/miniconda3" ]; then
-  echo "Miniconda not found in $WORK..."
-  echo "Installing..."
-  mkdir -p $WORK/miniconda3
-  curl https://repo.anaconda.com/miniconda/Miniconda3-py311_23.10.0-1-Linux-x86_64.sh -o $WORK/miniconda3/miniconda.sh
-  bash $WORK/miniconda3/miniconda.sh -b -u -p $WORK/miniconda3
-  rm -rf $WORK/miniconda3/miniconda.sh
-  export PATH="$WORK/miniconda3/bin:$PATH"
-  echo "Ensuring conda base environment is OFF..."
-  conda config --set auto_activate_base false
-else
-  export PATH="$WORK/miniconda3/bin:$PATH"
-fi
-
-conda init bash
-echo "Sourcing .bashrc..."
-source ~/.bashrc
-unset PYTHONPATH
-
-echo "Initializing conda..."
-conda info
-## Path to the python environment where the jupyter notebook packages are installed
-if [ ! -d "$git_repo_working_dir" ]; then
-    echo "Env not found, downloading"
-    git clone  https://github.com/In-For-Disaster-Analytics/sites-and-stories-nlp.git --branch jupyterenv $git_repo_working_dir
-else
-    mkdir -p ${cookbook_archive_dir}
-    git clone  https://github.com/In-For-Disaster-Analytics/sites-and-stories-nlp.git --branch jupyterenv ${cookbook_archive_dir}
-fi
-
-export TRANSFORMERS_CACHE="$git_repo_working_dir"
-
-echo "TACC: running on node $NODE_HOSTNAME_PREFIX on $NODE_HOSTNAME_DOMAIN"
-
-TAP_FUNCTIONS="/share/doc/slurm/tap_functions"
-if [ -f ${TAP_FUNCTIONS} ]; then
-    . ${TAP_FUNCTIONS}
-else
-    echo "TACC:"
-    echo "TACC: ERROR - could not find TAP functions file: ${TAP_FUNCTIONS}"
-    echo "TACC: ERROR - Please submit a consulting ticket at the TACC user portal"
-    echo "TACC: ERROR - https://portal.tacc.utexas.edu/tacc-consulting/-/consult/tickets/create"
-    echo "TACC:"
-    echo "TACC: job $SLURM_JOB_ID execution finished at: $(date)"
-    exit 1
-fi
-
-# Note: NotebookApp needs some work.
-JUPYTER_SERVER_APP="ServerApp"
-if [ ${JUPYTER_SERVER_APP} == "ServerApp" ]; then
-    JUPYTER_BIN="jupyter-lab"
-elif [ ${JUPYTER_SERVER_APP} == "NotebookApp" ]; then
-    JUPYTER_BIN="jupyter-notebook"
-fi
-echo "JUPYTER_SERVER_APP is $JUPYTER_SERVER_APP"
-echo "JUPYTER_BIN is $JUPYTER_BIN"
-
-NB_SERVERDIR=$HOME/MyData/.jupyter
-
-# make .jupyter dir for logs
-mkdir -p ${NB_SERVERDIR}
-
-mkdir -p ${HOME}/.tap # this should exist at this point, but just in case...
-TAP_CERTFILE=${HOME}/.tap/.${SLURM_JOB_ID}
-
-# bail if we cannot create a secure session
-if [ ! -f ${TAP_CERTFILE} ]; then
-    echo "TACC: ERROR - could not find TLS cert for secure session"
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-
-# bail if we cannot create a token for the session
-TAP_TOKEN=$(tap_get_token)
-if [ -z "${TAP_TOKEN}" ]; then
-    echo "TACC: ERROR - could not generate token for jupyter session"
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-echo "TACC: using token ${TAP_TOKEN}"
-
-# create the tap jupyter config if needed
-TAP_JUPYTER_CONFIG="$NB_SERVERDIR/jupyter_config.py"
-
-echo ${PWD}
-
-
-cat <<- EOF > ${TAP_JUPYTER_CONFIG}
-# Configuration file for TAP jupyter session
-import ssl
-c = get_config()
-c.IPKernelApp.pylab = "inline"  # if you want plotting support always
-c.${JUPYTER_SERVER_APP}.ip = "0.0.0.0"
-c.${JUPYTER_SERVER_APP}.port = $LOCAL_PORT
-c.${JUPYTER_SERVER_APP}.open_browser = False
-c.${JUPYTER_SERVER_APP}.allow_origin = u"*"
-c.${JUPYTER_SERVER_APP}.ssl_options = {"ssl_version": ssl.PROTOCOL_TLSv1_2}
-c.${JUPYTER_SERVER_APP}.root_dir = "${_tapisJobWorkingDir}"
-c.${JUPYTER_SERVER_APP}.preferred_dir = "${_tapisJobWorkingDir}"
-c.${JUPYTER_SERVER_APP}.notebook_dir = "${_tapisJobWorkingDir}/work"
-c.FileContentsManager.delete_to_trash = False
-c.IdentityProvider.token = "${TAP_TOKEN}"
-c.MultiKernelManager.default_kernel_name = 'llm'
-EOF
-
-# launch jupyter
-JUPYTER_LOGFILE=${NB_SERVERDIR}/${NODE_HOSTNAME_PREFIX}.log
-touch $JUPYTER_LOGFILE
-
-JUPYTER_ARGS="--certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG}"
-echo "TACC: using jupyter command: ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} &"
-nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} &
-
-JUPYTER_PID=$!
-
-LOGIN_PORT=$(tap_get_port)
-echo "TACC: got login node jupyter port ${LOGIN_PORT}"
-
-JUPYTER_URL="https://${NODE_HOSTNAME_DOMAIN}:${LOGIN_PORT}/?token=${TAP_TOKEN}"
-
-
-# verify jupyter is up. if not, give one more try, then bail
-if ! $(ps -fu ${USER} | grep ${JUPYTER_BIN} | grep -qv grep) ; then
-    # sometimes jupyter has a bad day. give it another chance to be awesome.
-    echo "TACC: first jupyter launch failed. Retrying..."
-    nohup ${JUPYTER_BIN} ${JUPYTER_ARGS} &> ${JUPYTER_LOGFILE} &
-fi
-
-if ! $(ps -fu ${USER} | grep ${JUPYTER_BIN} | grep -qv grep) ; then
-    # jupyter will not be working today. sadness.
-    echo "TACC: ERROR - jupyter failed to launch"
-    echo "TACC: ERROR - this is often due to an issue in your python or conda environment"
-    echo "TACC: ERROR - jupyter logfile contents:"
-    cat ${JUPYTER_LOGFILE}
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-
-
-
-# Port forwarding is set up for the four login nodes.
-#
-#   f: Requests ssh to go to background just before command execution.
-#      Used if ssh asks for passwords but the user wants it in the background. Implies -n too.
-#   g: Allows remote hosts to connect to local forwarded ports
-#   N: Do not execute a remote command. Useful for just forwarding ports.
-#   R: Connections to given TCP port/Unix socket on remote (server) host forwarded to local side.
-#
-# Create a reverse tunnel port from the compute node to the login nodes.
-# Make one tunnel for each login so the user can just connect to stampede.tacc.utexas.edu.
-
-# Disable exit on error so we can check the ssh tunnel status.
-set +e
-for i in $(seq 2); do
-    ssh -o StrictHostKeyChecking=no -q -f -g -N -R ${LOGIN_PORT}:${NODE_HOSTNAME_PREFIX}:${LOCAL_PORT} login${i}
-done
-if [ $(ps -fu ${USER} | grep ssh | grep login | grep -vc grep) != 2 ]; then
-    # jupyter will not be working today. sadness.
-    echo "TACC: ERROR - ssh tunnels failed to launch"
-    echo "TACC: ERROR - this is often due to an issue with your ssh keys"
-    echo "TACC: ERROR - undo any recent mods in ${HOME}/.ssh"
-    echo "TACC: ERROR - or submit a TACC consulting ticket with this error"
-    echo "TACC: job ${SLURM_JOB_ID} execution finished at: $(date)"
-    exit 1
-fi
-# Re-enable exit on error.
-set -e
-
-INTERACTIVE_WEBHOOK_URL="${_webhook_base_url}"
-
-# Wait a few seconds for jupyter to boot up and send webhook callback url for job ready notification.
-# Notification is sent to _INTERACTIVE_WEBHOOK_URL, e.g. https://3dem.org/webhooks/interactive/
-(
-    sleep 5 &&
-    curl -k --data "event_type=interactive_session_ready&address=${JUPYTER_URL}&owner=${_tapisJobOwner}&job_uuid=${_tapisJobUUID}" "${_INTERACTIVE_WEBHOOK_URL}" &
-) &
-
-# Delete the session file to kill the job.
-echo $NODE_HOSTNAME_LONG $IPYTHON_PID > $SESSION_FILE
-
-### Create env
-if { conda env list | grep 'llm'; } >/dev/null 2>&1; then
-    conda activate llm
-else
-    conda env create -n llm -f $git_repo_working_dir/.binder/environment.yml --force
-    conda activate llm
-    pip install --no-cache-dir -r $git_repo_working_dir/.binder/requirements.txt
-    python -m ipykernel install --user --name llm --display-name "Python (llm)"
-fi
-echo "JUPYTER_URL is"
-echo  $JUPYTER_URL
-
-# While the session file remains undeleted, keep Jupyter session running.
-while [ -f $SESSION_FILE ] ; do
-    sleep 10
-done
+install_conda
+load_cuda
+export_repo_variables
+init_directory
+load_tap_functions
+get_tap_certificate
+get_tap_token
+create_jupyter_configuration
+port_fowarding
+install_dependencies
+run_jupyter
+send_url_to_webhook
+session_cleanup
