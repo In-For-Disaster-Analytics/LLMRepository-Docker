2fb9d68b83fe542e6b11ee259825ea6813d2eca0
Will Mobley <feulmob+github@gmail.com>
2024-09-13 10:05:24 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -159,7 +159,7 @@ function run_jupyter() {
 	NB_SERVERDIR=$HOME/.jupyter
 	JUPYTER_SERVER_APP="ServerApp"
 	JUPYTER_BIN="jupyter-lab"
-	JUPYTER_ARGS="--certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG} --notebook-dir='${COOKBOOK_WORKSPACE_DIR}' --preferred-dir='${COOKBOOK_WORKSPACE_DIR}'"
+	JUPYTER_ARGS="--certfile=$(cat ${TAP_CERTFILE}) --config=${TAP_JUPYTER_CONFIG} --notebook-dir='${COOKBOOK_DIR}' --preferred-dir='${COOKBOOK_WORKSPACE_DIR}'"
 	JUPYTER_LOGFILE=${NB_SERVERDIR}/${NODE_HOSTNAME_PREFIX}.log
 	mkdir -p ${NB_SERVERDIR}
 	touch $JUPYTER_LOGFILE
